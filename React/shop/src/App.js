ccd098e4698531731398a35a923acd07fa176339
김륜하 <ryoonzz@gmail.com>
2022-10-20 12:55:47 +0900

    allupdate

--- React/shop/src/App.js
+++ React/shop/src/App.js
@@ -1,25 +1,28 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap/Button';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoesShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#pricing">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
       <br />
+
+    <div className='main-bg'></div>
+
+    <div></div>
+
     </div>
   );
 }
