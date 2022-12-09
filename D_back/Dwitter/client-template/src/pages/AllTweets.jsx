ae0b7747084377cb7ad932bb3a7f374851315dbe
김륜하 <kimryoonha@ryoon-MacBookPro.local>
2022-12-09 22:11:27 +0900

    🔥delete:파일정리

--- D_back/Dwitter/client-template/src/pages/AllTweets.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Tweets from '../components/Tweets';
-
-const AllTweets = ({ tweetService }) => (
-  <Tweets tweetService={tweetService} addable={true} />
-);
-
-export default AllTweets;
